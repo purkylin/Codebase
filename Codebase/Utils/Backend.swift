244cf150a01021e9c5f21df107052b0b5b6fef28
Purkylin King <purkylin@gmail.com>
2020-06-20 18:10:05 +0800

    add backend

---
+++ Codebase/Utils/Backend.swift
@@ -0,0 +1,14 @@
+//
+//  Backend.swift
+//  Codebase
+//
+//  Created by Purkylin King on 2020/6/20.
+//  Copyright © 2020 Purkylin King. All rights reserved.
+//
+
+import Foundation
+
+enum Backend {
+    case qa
+    case product
+}
